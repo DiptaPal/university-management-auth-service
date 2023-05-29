4b99a86ed16e3f576feaa6d3d2b1bc65bdff083c
Dipta Pal <diptapal1998@gmail.com>
2023-05-29 14:16:32 +0600

    [DONE] setup expres, mongoose, cors, parser

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import config from "./config/index";
+import app from "./app";
+
+async function main() {
+  try {
+    await mongoose.connect(config.mongoURI as string);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
+app.listen(config.port, () => {
+  console.log(`server is listening on port ${config.port}`);
+});
